44de7a09d28db7dc174788b4b5a80d152739ceea
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-09-17 23:18:01 +0530

    footer added

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/frontend_assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt=''/>
+                <p>Delivering fresh, high-quality meals to your doorstep with reliability and care.Our mission is to provide exceptional service and a seamless dining experience.Trust us to bring convenience and flavor together, every time you order.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt=""/>
+                    <img src={assets.twitter_icon} alt=""/>
+                    <img src={assets.linkedin_icon} alt=""/>
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>(+96) 12 345 6789</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr/>
+        <p className='footer-copyright'>Copyright 2025 © Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
