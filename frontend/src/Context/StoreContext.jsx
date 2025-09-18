829818164c4fd0d129ac24da0d6672ecff3b7856
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-09-18 14:12:43 +0530

    cart

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -3,37 +3,46 @@ import { food_list } from "../assets/frontend_assets/assets";
 
 export const StoreContext = createContext(null)
 
-const StoreContextProvider = (props ) => {
+const StoreContextProvider = (props) => {
 
-    const [cartItems,setCartItems] = useState({});
+    const [cartItems, setCartItems] = useState({});
 
-    const addtoCart =(itemId)=>{
+    const addtoCart = (itemId) => {
         if (!cartItems[itemId]) {
-            setCartItems((prev)=>({...prev,[itemId]:1}))
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
-        else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
     }
 
-    const removeFromCart = (itemId) =>{
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+
+        }
+        return totalAmount;
+    }
 
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addtoCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
 
     return (
-        <StoreContext.Provider value = {contextValue}>
+        <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
     )
