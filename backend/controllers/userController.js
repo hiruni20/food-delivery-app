5af0a9a84730c996c1df1b0d66349d722f104dbe
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-09-29 20:45:29 +0530

    userrouter

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,3 +4,13 @@ import bcrypt from "bcrypt"
 import  validator from "validator"
 
 //login user
+const loginUser = async (req,res) => {
+
+}
+
+//register user
+const registerUser = async (req,res) => {
+
+}
+
+export {loginUser,registerUser}
