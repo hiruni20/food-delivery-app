27ec9f007cc01609f2e7029289b54917b4c9d833
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-09-30 23:13:47 +0530

    integrate api with frontend

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/frontend_assets/assets'
 
 const LoginPopup = ({ setShowLogin }) => {
 
-    const [currState, setCurrState] = useState("Sign Up")//Sign Up
+    const [currState, setCurrState] = useState("Login")//Sign Up
+    const [data,setData] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+    }
+
+    useEffect(() => {
+        console.log(data);
+    },[data])
+
     return (
         <div className='login-popup'>
             <form className='login-popup-container'>
@@ -13,10 +29,10 @@ const LoginPopup = ({ setShowLogin }) => {
                     <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt='' />
                 </div>
                 <div className="login-popup-inputs">
-                    {currState === "Login" ? <></> : <input type='text' placeholder='Your name' required />}
+                    {currState === "Login" ? <></> : <input name="name" onChange={onChangeHandler} value={data.name} type='text' placeholder='Your name' required />}
 
-                    <input type='emai' placeholder='Your email' required />
-                    <input type='password' placeholder='password' required />
+                    <input name="email" onChange={onChangeHandler} value={data.email} type='emai' placeholder='Your email' required />
+                    <input name="password" onChange={onChangeHandler} value={data.password}type='password' placeholder='password' required />
                     
                 </div>
                 <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
