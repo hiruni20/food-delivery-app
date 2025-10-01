e6acc1822a795f106170cda8917b9f1bff3ec977
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-10-01 11:43:58 +0530

    fetch images

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/frontend_assets/assets";
+
 
 export const StoreContext = createContext(null)
 
@@ -7,7 +8,9 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
     const url = "http://localhost:4000"
-    const [token,setToken] = useState("")
+    const [token,setToken] = useState("");
+    const [food_list,setFoodList] = useState([])
+
 
     const addtoCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -34,10 +37,20 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    const fetchFoodList = async () => {
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data)
+    }
+
     useEffect(() => {
-        if (localStorage.getItem("token")) {
+        
+        async function loadData() {
+           await fetchFoodList() 
+           if (localStorage.getItem("token")) {
             setToken(localStorage.getItem("token"));
         }
+        }
+        loadData();
     },[])
 
     const contextValue = {
